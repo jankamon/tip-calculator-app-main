1f234f540b46f11caad618d135dcd71dd7f049f8
janekk <janekkamon@gmail.com>
2023-03-02 19:18:18 +0100

    added input validation

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const tipAmountValue = document.getElementById('tip-amount');
 const total = document.getElementById('total');
 const resetBtn = document.getElementById('reset');
+const peopleInput = document.getElementById('number-of-people');
+const errorText = document.getElementById('input-error');
 
 let billValue = 0;
 let tipValue = 0;
@@ -11,11 +13,10 @@ let totalAmount = 0;
 const reset = () => {
     document.getElementById('bill').value = '';
     document.getElementById('tip-percent').value = '';
-    document.getElementById('number-of-people').value = '';
+    peopleInput.value = '';
     tipAmountValue.innerHTML = '$0.00';
     total.innerHTML = '$0.00';
     billValue, tipValue, numOfPeople, tipAmount, totalAmount = 0;
-
 }
 
 const unlockReset = () => {
@@ -24,6 +25,7 @@ const unlockReset = () => {
 
 const countTipAmount = () => {
     if(numOfPeople > 0) {
+    errorText.classList.remove('show-error');
     //counts and sets tip amount
     tipAmount = billValue * (tipValue/100) / numOfPeople;
     tipAmountValue.innerHTML = '$' + tipAmount.toFixed(2);
@@ -32,6 +34,8 @@ const countTipAmount = () => {
     total.innerHTML = '$' + totalAmount.toFixed(2);
 
     unlockReset();
+    } else if(numOfPeople < 0) {
+        errorText.classList.add('show-error');
     }
 }
 
